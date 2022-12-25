e9d33d051ce63ed804d19105a6c05c1b549f77dc
Jeremy Black <jmckib2@gmail.com>
2022-12-24 20:53:43 -0800

    Add nickname, date, and author in italics.
    Also send a new message every day, resetting after 6 months.

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 ZIP_PKG_NAME = deployment_package.zip
 
-.PHONY: install_deps
-install_deps:
+.PHONY: deps
+deps:
 	rm -rf lambda_function/package || true
 	pip install --target lambda_function/package requests pyyaml
 
-.PHONY: build_zip
-build_zip:
+.PHONY: zip
+zip:
 	rm my-deployment-package.zip || true
 	(cd lambda_function/package/ && zip -r ../../$(ZIP_PKG_NAME) .)
-	(cd lambda_function/ && zip ../$(ZIP_PKG_NAME) lambda_function.py secrets.yaml quotes.json)
+	(cd lambda_function/ && zip ../$(ZIP_PKG_NAME) lambda_function.py secrets.yaml quotes.json nicknames.json)
 
 .PHONY: build
-build: install_deps build_zip
+build: deps zip
--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -1,4 +1,6 @@
+from datetime import datetime, timezone, timedelta
 import json
+import random
 import requests
 import yaml
 try:
@@ -6,11 +8,41 @@ try:
 except ImportError:
     from yaml import Loader, Dumper
 
+
+def _day_suffix(n):
+    return "th" if 4<=n%100<=20 else {1:"st",2:"nd",3:"rd"}.get(n%10, "th")
+
+
 def lambda_handler(event, context):
     secrets = yaml.load(open('secrets.yaml'), Loader=Loader)
 
-    message = json.load(open('quotes.json'))[100]['quote']
+    SEED = 1
+    random.seed(SEED)
+    quotes = json.load(open("quotes.json"))
+    quotes = sorted(quotes, key=lambda i: random.random())
+
+    nicknames = json.load(open("nicknames.json"))
+    nicknames = sorted(nicknames, key=lambda i: random.random())
+
+    # Get today's date in the Pacific timezone
+    timezone_offset = -8.0  # Pacific Standard Time (UTC−08:00)
+    tzinfo = timezone(timedelta(hours=timezone_offset))
+    today = datetime.now(tzinfo).date()
+
+    # Start over every 6 months.
+    tm_yday = today.timetuple().tm_yday
+    quote_dict = quotes[(tm_yday - 1) % len(quotes)]
+    nickname = nicknames[(tm_yday - 1) % len(nicknames)]
+
+    # Build the message
+    date_str = (today.strftime('%B %d{th}, %Y')
+                .replace('{th}', _day_suffix(today.day)))
+    msg = (f"Good morning {nickname},\n\nHere's your daily dose of lurv for "
+           f"{date_str}:\n\n{quote_dict['quote']}")
+    if "author" in quote_dict:
+        msg += f"\n\n<i>- {quote_dict['author']}</i>"
+
     url = (f"https://api.telegram.org/bot{secrets['TELEGRAM_TOKEN']}/"
-           f"sendMessage?chat_id={secrets['USER_CHAT_ID']}&text={message}")
+           f"sendMessage?chat_id={secrets['USER_CHAT_ID']}&text={msg}&parse_mode=html")
     # Send the message
     print(requests.get(url).json())
